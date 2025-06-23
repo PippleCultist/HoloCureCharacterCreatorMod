ed920da7524e4f2b28ea27b18c1efc920f98d80d
PippleCultist <traykickcopestay@gmail.com>
2025-06-22 20:57:11 -0500

    Updated version num

--- HoloCureCharacterCreatorMod/source/ModuleMain.h
+++ HoloCureCharacterCreatorMod/source/ModuleMain.h
@@ -3,7 +3,7 @@
 #include <YYToolkit/YYTK_Shared.hpp>
 #include <CallbackManager/CallbackManagerInterface.h>
 
-#define VERSION_NUM "v1.0.5"
+#define VERSION_NUM "v1.1.2"
 #define MODNAME "Holocure Character Creator Mod " VERSION_NUM
 
 #define SOME_ENUM(DO) \
